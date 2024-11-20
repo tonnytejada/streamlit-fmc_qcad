b490e5313f29abf8521522768f946b4f83fc5fe4
tonnytejada <36516865+tonnytejada@users.noreply.github.com>
2024-11-19 19:32:44 -0500

    about_me.py

---
+++ scrpt/views/about_me.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+#with col1:
+#    st.image("./assets/profile_image.png", width=230)
+
+with col2:
+    st.title("Alex Delgado", anchor=False)
+    st.write(
+        "**Resouces Geologist Senior and Data Analyst**"
+    )
+    st.write(
+        "Email: "
+    )
